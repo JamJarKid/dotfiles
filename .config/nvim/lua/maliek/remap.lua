ecf0ea5577316a4a10a7e8da5583666c98c228d1
malijah <41331314+Malijah-C@users.noreply.github.com>
2023-12-03 17:47:40 +0000

    added new remaps

--- .config/nvim/lua/maliek/remap.lua
+++ .config/nvim/lua/maliek/remap.lua
@@ -12,3 +12,6 @@ vim.keymap.set("n", "<leader>y", "+y")
 vim.keymap.set("v", "<leader>y", "+y")
 vim.keymap.set("n", "<leader>Y", "+Y")
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
