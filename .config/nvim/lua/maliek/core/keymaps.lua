fbcda695745f7a9cba6e68f17ce8741964cd908b
malijah <41331314+Malijah-C@users.noreply.github.com>
2023-12-18 23:12:23 +0000

    changed name for remap

--- .config/nvim/lua/maliek/core/keymaps.lua
+++ .config/nvim/lua/maliek/core/keymaps.lua
@@ -1,16 +1,18 @@
+local remap = vim.keymap.set
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>chr", '<cmd>!open -a "Google Chrome" %<CR>')
-vim.keymap.set("n", "x", '"_x')
-vim.keymap.set("n", "<leader>o", "o<Esc>", { silent = true, desc = "Insert new line below and exit" })
-vim.keymap.set("n", "<leader>O", "O<Esc>", { silent = true, desc = "Insert new line above and exit" })
-vim.keymap.set("n", "<leader>sv", "<C-w>v")
-vim.keymap.set("n", "<leader>sh", "<C-w>s")
-vim.keymap.set("n", "<leader>sx", ":close<CR>")
 
-vim.keymap.set("n", "<leader>y", "+y")
-vim.keymap.set("v", "<leader>y", "+y")
-vim.keymap.set("n", "<leader>Y", "+Y")
+remap("n", "<leader>pv", vim.cmd.Ex)
+remap("n", "<leader>chr", '<cmd>!open -a "Google Chrome" %<CR>')
+remap("n", "x", '"_x')
+remap("n", "<leader>o", "o<Esc>", { silent = true, desc = "Insert new line below and exit" })
+remap("n", "<leader>O", "O<Esc>", { silent = true, desc = "Insert new line above and exit" })
+remap("n", "<leader>sv", "<C-w>v")
+remap("n", "<leader>sh", "<C-w>s")
+remap("n", "<leader>sx", ":close<CR>")
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+remap("n", "<leader>y", "+y")
+remap("v", "<leader>y", "+y")
+remap("n", "<leader>Y", "+Y")
+
+remap("n", "<C-d>", "<C-d>zz")
+remap("n", "<C-u>", "<C-u>zz")
