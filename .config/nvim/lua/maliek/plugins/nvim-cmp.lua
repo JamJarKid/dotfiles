91ae1b92fd3c49988f7344f339712881738bb44d
malijah <41331314+Malijah-C@users.noreply.github.com>
2024-01-06 20:46:51 +0000

    removed enter shortcut from nvim-cmp and added new keymaps to harpoon

--- .config/nvim/lua/maliek/plugins/nvim-cmp.lua
+++ .config/nvim/lua/maliek/plugins/nvim-cmp.lua
@@ -18,14 +18,7 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        ['<Tab>'] = function(fallback)
-            if luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end,
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
         ['<S-Tab>'] = function(fallback)
             if luasnip.jumpable(-1) then
                 luasnip.jump(-1)
