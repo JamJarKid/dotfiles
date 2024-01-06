cb3d0515e6ad4158c409f22da5b1994cf942e685
malijah <41331314+Malijah-C@users.noreply.github.com>
2024-01-06 23:07:10 +0000

    added more keymaps for lsp including definition hover and diagnostics

--- .config/nvim/lua/maliek/plugins/lsp.lua
+++ .config/nvim/lua/maliek/plugins/lsp.lua
@@ -14,6 +14,14 @@ lsp.ensure_installed({
     'cssls',
 })
 
+lsp.on_attach(function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+end)
+
 lsp.setup()
 
 lspconfig.pyright.setup({})
