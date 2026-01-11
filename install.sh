1c48bf3d9a5c4bd30d7fe47701cfbc6cd4d0a2de
JamJarKid <41331314+Malijah-C@users.noreply.github.com>
2026-01-11 00:03:10 +0000

    added a check to install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 # Housekeeping
 
-mkdir ~/.config
-
+if [ ! -d ~/.config]; then
+    mkdir ~/.config
+else
+    echo "~/.config exists skipping creation"
+fi
 casks=(
     "ghostty"
     "visual-studio-code"
@@ -29,12 +32,16 @@ do
         read -n 1 -s -r -p "Press Any Key after you finish setting-up the current app"
     fi
 done
+
+# git
+ln -s "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/dotfiles/.gitignore_global" "$HOME/.gitignore_global"
     
 # ghostty config
-
 mkdir ~/.config/ghostty
 ln -s "$HOME/dotfiles/.config/ghostty" "$HOME/.config/ghostty"
 
 # karabiner-elements
-
 ln -s "$HOME/dotfiles/.config/karabiner" "$HOME/.config/karabiner" 
+
+
